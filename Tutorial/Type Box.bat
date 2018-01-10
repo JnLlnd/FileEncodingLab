08569b85a9f2ad1ccb6648f24c77aa9520eabcce
Jean Lalonde <courriel@jeanlalonde.ca>
2018-01-09 22:59:38 -0500

    moved files to Tutorial subfolder

---
+++ Tutorial/Type Box.bat
@@ -0,0 +1,11 @@
+@cls
+@echo Changing the DOS command window to code page 1252 (Windows Latin 1 - ANSI)
+chcp 1252
+pause
+type box-cp437.txt
+pause
+@echo Changing to code page 437 (MS DOS Latin US - Extended ASCII)
+chcp 437
+pause
+type box-cp437.txt
+pause
--- Type Box.bat
+++
@@ -1,6 +0,0 @@
-type box-cp437.txt
-chcp 1252
-type box-cp437.txt
-chcp 437
-type box-cp437.txt
-pause
